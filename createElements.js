d41084a624471d42c09b6cd1262fff8db90c2888
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-03-14 02:59:52 -0300

    Sort and filter methods

--- createElements.js
+++ createElements.js
@@ -1,40 +1,53 @@
-function updateTable(employees, roles) {
+function emptyResult() {
   const resultElement = document.getElementById('result');
-  resultElement.innerHTML = '';
-  const tableElement = document.createElement('table');
-  resultElement.appendChild(tableElement);
-  //Head
-  const theadElement = document.createElement('thead');
-  tableElement.appendChild(theadElement);
-  const trHeadElement = document.createElement('tr');
-  theadElement.appendChild(trHeadElement);
-  ['ID', 'Name', 'Role', 'Salary (R$)'].map((element) => {
-    const tdElement = document.createElement('td');
-    tdElement.innerHTML = element;
-    trHeadElement.appendChild(tdElement);
-  });
-  //Body
-  const tbodyElement = document.createElement('tbody');
-  tableElement.appendChild(tbodyElement);
-  employees.map((element) => {
-    const trBodyElement = document.createElement('tr');
-    tbodyElement.appendChild(trBodyElement);
-    [element.id, element.name, element.role_id, element.salary].map(
-      (field, index) => {
-        let value = field;
-        if (index == 2) {
-          value = roles
-            .filter((r) => r.id == element.role_id)
-            .map((r) => r.name)[0];
-        } else if (index == 3) {
-          value = parseFloat(value).toFixed(2);
+  resultElement.innerHTML = '<p>0 results for the search.</p>';
+}
+
+function updateTable(employees, roles) {
+  //If employees json is empty
+  if (employees.length == 0) {
+    emptyResult();
+    return;
+  }
+  //If has employees on json
+  else {
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = '';
+    const tableElement = document.createElement('table');
+    resultElement.appendChild(tableElement);
+    //Head
+    const theadElement = document.createElement('thead');
+    tableElement.appendChild(theadElement);
+    const trHeadElement = document.createElement('tr');
+    theadElement.appendChild(trHeadElement);
+    ['ID', 'Name', 'Role', 'Salary (R$)'].map((element) => {
+      const tdElement = document.createElement('td');
+      tdElement.innerHTML = element;
+      trHeadElement.appendChild(tdElement);
+    });
+    //Body
+    const tbodyElement = document.createElement('tbody');
+    tableElement.appendChild(tbodyElement);
+    employees.map((element) => {
+      const trBodyElement = document.createElement('tr');
+      tbodyElement.appendChild(trBodyElement);
+      [element.id, element.name, element.role_id, element.salary].map(
+        (field, index) => {
+          let value = field;
+          if (index == 2) {
+            value = roles
+              .filter((r) => r.id == element.role_id)
+              .map((r) => r.name)[0];
+          } else if (index == 3) {
+            value = parseFloat(value).toFixed(2);
+          }
+          const tdElement = document.createElement('td');
+          tdElement.innerHTML = value;
+          trBodyElement.appendChild(tdElement);
         }
-        const tdElement = document.createElement('td');
-        tdElement.innerHTML = value;
-        trBodyElement.appendChild(tdElement);
-      }
-    );
-  });
+      );
+    });
+  }
   //Span count
   const countElement = document.getElementById('count');
   countElement.innerHTML = employees.length;
--- handleFunctions.js
+++ handleFunctions.js
@@ -1,12 +1,75 @@
 let employees, roles;
-let currentEmployees, currentRoles;
+let currentEmployees;
+
+//Sorting the current list
+function sortBy(value) {
+  switch (parseInt(value)) {
+    //Name ascending
+    case 0:
+      currentEmployees = currentEmployees.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+      break;
+    //Name descending
+    case 1:
+      currentEmployees = currentEmployees.sort((a, b) =>
+        b.name.localeCompare(a.name)
+      );
+      break;
+    //Salary ascending
+    case 2:
+      currentEmployees = currentEmployees.sort((a, b) => b.salary - a.salary);
+      break;
+    //Salary descending
+    case 3:
+      currentEmployees = currentEmployees.sort((a, b) => a.salary - b.salary);
+      break;
+  }
+  updateTable(currentEmployees, roles);
+}
 
 //Fetch when window load
 window.addEventListener('load', async () => {
   employees = await fetchAPI('http://localhost:3000/employees');
   roles = await fetchAPI('http://localhost:3000/roles');
   currentEmployees = employees;
-  currentRoles = roles;
+  sortBy(0);
   updateTable(employees, roles);
   updateFilter(roles);
+  checkBoxEvent();
 });
+
+//Listen the select input
+document.getElementById('sortBy').addEventListener('change', (event) => {
+  const { value } = event.target;
+  sortBy(value);
+});
+
+//Add a eventlistener for each checkbox
+function checkBoxEvent() {
+  const allCheckBoxElements = document.getElementsByName('checkboxFilter');
+  for (let i = 0; i < roles.length; i++) {
+    allCheckBoxElements[i].addEventListener('change', () => {
+      //Get list of all checked
+      const checkedElements = Array.from(
+        document.querySelectorAll('input:checked')
+      );
+      //If none checked
+      if (checkedElements.length === 0) currentEmployees = employees;
+      //If someone checked
+      else {
+        //Get all checked values
+        const listCheckedValues = checkedElements.map((element) =>
+          parseInt(element.value)
+        );
+        //Using the array values to filter
+        currentEmployees = employees.filter((element) => {
+          return listCheckedValues.includes(parseInt(element.role_id));
+        });
+      }
+      //Sorting again
+      const selectValue = document.getElementById('sortBy').value;
+      sortBy(selectValue);
+    });
+  }
+}
